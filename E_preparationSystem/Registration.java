9afc0a1291e4964d2fb4c14bce0a3dc3a035efb5
Avantika Tayde <103492326+AvantikaTayde123@users.noreply.github.com>
2024-11-08 23:14:16 +0530

    Add files via upload

---
+++ E_preparationSystem/Registration.java
@@ -0,0 +1,35 @@
+package E_preparationSystem;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class Registration{
+    public static void registration(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Username");
+        String username = sc.nextLine();
+        System.out.println("Enter Password");
+        String password = sc.nextLine();
+
+        if(password.length()<8){
+        System.out.println("Password must be at least 8 characters long! Try again");
+        return;
+        }
+
+
+        Connection con = connection.getConnection();
+        String sql = "insert into users(username,password)values(?,?)";
+
+        try{
+            PreparedStatement pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, username);
+            pstmt.setString(2, password);
+            pstmt.executeUpdate();
+            System.out.println("User Registrered Successfully");
+        }catch(SQLException e){
+         e.printStackTrace();
+        }
+
+    }
+}
---
+++ E_preparationSystem/connection.java
@@ -0,0 +1,26 @@
+package E_preparationSystem;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+// Global connection Class
+public class connection {
+	static Connection con; 
+	public static Connection getConnection()
+	{
+		try {
+			
+			
+			String mysqlJDBCDriver= "com.mysql.cj.jdbc.Driver"; 
+			String url= "jdbc:mysql://localhost:3306/Epreparation"; 
+			String user = "root";	 
+			String pass = "root"; 
+			Class.forName(mysqlJDBCDriver);
+			con = DriverManager.getConnection(url, user, pass);
+		}
+		catch (Exception e) {
+			System.out.println("Connection Failed!");
+		}
+
+		return con;
+	}
+}
